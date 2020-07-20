6b0717887f8b0dd9e26b898d33bcabdb6826f4bb
beata <beata@emc2wave.com>
2020-07-20 15:53:54 -0700

    add window.SpeechRecognition

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const msgEl = document.getElementById(msg);
 
 const randomNum = getRandomNuber();
 
+console.log('Number:', randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
 function getRandomNuber() {
   return Math.floor(Math.random() * 100) + 1;
 }
