2e08b8c76824b82ae07a6c0d2af5c81e261d3774
beata <beata@emc2wave.com>
2020-07-20 17:19:51 -0700

    Capture user speak

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const msgEl = document.getElementById(msg);
+const msgEl = document.getElementById('msg');
 
 const randomNum = getRandomNuber();
 
@@ -12,6 +12,22 @@ let recognition = new window.SpeechRecognition();
 //Start recognition and game
 recognition.start();
 
+// Capture user speak
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+
+  writeMessage(msg);
+  // checkNumber(msg);
+}
+
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// generate random number
 function getRandomNuber() {
   return Math.floor(Math.random() * 100) + 1;
 }
